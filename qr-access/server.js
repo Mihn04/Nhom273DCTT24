1d73349487beafa396b03cc4db24f26aa35b7df0
hoanghuy <brlt091@gmail.com>
2025-04-23 17:06:19 +0700

    1

--- qr-access/server.js
+++ qr-access/server.js
@@ -19,3 +19,26 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+const os = require('os');
+
+// Hàm lấy IP LAN
+function getLocalIp() {
+    const interfaces = os.networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+        for (const iface of interfaces[name]) {
+            if (iface.family === 'IPv4' && !iface.internal && iface.address.startsWith('192.168.')) {
+                return iface.address;
+            }
+        }
+    }
+    return 'localhost';
+}
+
+// Trả file config.js động
+app.get('/config.js', (req, res) => {
+    const ip = getLocalIp();
+    res.type('application/javascript');
+    res.send(`const QR_SERVER_IP = "${ip}";`);
+});
+
